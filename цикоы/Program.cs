397c1ab7b62d167ce8d9312ae16cc7a856ee8f27
lazineeesss <keton116rus@mail.ru>
2022-09-27 11:14:30 +0300

    Добавьте файлы проекта.

---
+++ цикоы/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace project
+{
+
+    class Program
+    {
+
+        static void Main()
+        {
+            /* for(int i = 200; i > 10; i++)
+             {
+                 Console.WriteLine("элемент: {0}", i);
+
+             }*/
+            /*  int i=0; 
+               while(i <= 10)
+               {
+                   Console.WriteLine("элемент: {0}", i);
+                   i++;    
+               }*/
+
+            /* bool isHasCar = true;
+             while (isHasCar)
+             {
+                 string end = Console.ReadLine();
+                 if(end == "end")
+                     isHasCar = false;
+             }*/
+           /* byte i = 100;
+            do
+            {
+                Console.WriteLine("element: " + i);
+            } while (i <10);*/
+
+            for(short i = 0; i < 10; i++)
+            {
+
+                //if(i >5 ) break;
+
+                if(i%2 == 0)
+                    continue;
+
+                Console.WriteLine("element" +i);
+            }
+
+        }
+
+    }
+
+}
